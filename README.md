efce65562af273baa8ffdd8eeb996160f2401a0a
Vishal Devadiga <vishaldevadiga003@gmail.com>
2023-06-20 00:38:15 +0530

    Finish backend?

---
+++ README.md
@@ -0,0 +1,6 @@
+# Just a web3 crowdfunding site
+
+Built backend using thirdweb framework. Used Solidity to code the smartcontract.
+
+Deployed it on sepolia test network because Goerli requires 0.01 eth on mainnet :(
+
--- backend/contracts/Contract.sol
+++ backend/contracts/Contract.sol
@@ -17,4 +17,60 @@ contract crowdfunding {
     mapping(uint256 => Campaign) public campaigns;
     uint256 public size_campaigns = 0;
 
+    function Create_Campaign(
+        address _owner,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image,
+        string memory _title,
+        string memory _desc
+    ) public returns (uint256) {
+        Campaign storage _campaign = campaigns[size_campaigns];
+
+        // Checks
+        require(block.timestamp > _deadline, "Not a valid deadline");
+        require(_target < 0, "Not a valid target");
+
+        _campaign.owner = _owner;
+        _campaign.target = _target;
+        _campaign.deadline = _deadline;
+        _campaign.image = _image;
+        _campaign.title = _title;
+        _campaign.desc = _desc;
+        _campaign.curr_amount = 0;
+
+        size_campaigns++;
+        return size_campaigns - 1;
+    }
+
+    function Donate_Campaign(uint256 _id) public payable {
+        uint256 _amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        (bool sent, ) = payable(campaign.owner).call{value: _amount}("");
+
+        if (sent == true) {
+            campaign.curr_amount += _amount;
+            campaign.donaters.push(msg.sender);
+            campaign.donations.push(_amount);
+        }
+    }
+
+    function Get_Donaters_Campaign(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donaters, campaigns[_id].donations);
+    }
+
+    function Get_Campaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory _campaigns = new Campaign[](size_campaigns);
+
+        for (uint i = 0; i < size_campaigns; i++) {
+            Campaign storage _campaign = campaigns[i];
+            _campaigns[i] = _campaign;
+        }
+
+        return (_campaigns);
+    }
 }
